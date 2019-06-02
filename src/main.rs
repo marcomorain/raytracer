c1ae4781b80638ba13ca32de236f44dc4f55c337
Marc O'Morain <marc@circleci.com>
2019-06-02 20:26:54 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,14 @@ use raytracer::tuple::*;
 fn tick(env: &Environment, proj: &Projectile) -> Projectile {
     return Projectile {
         position: &proj.position + &proj.velocity,
-        velocity: &proj.velocity + &(&env.gravity + &env.wind)
+        velocity: &proj.velocity + &(&env.gravity + &env.wind),
     };
 }
 
 fn main() {
-
-
     let env = Environment {
         gravity: vector(0.0, -0.1, 0.0),
-        wind: vector(-0.01, 0.0, 0.0)
+        wind: vector(-0.01, 0.0, 0.0),
     };
 
     let mut p = Projectile {
@@ -23,8 +21,8 @@ fn main() {
     };
 
     while p.position.1 > 0.0 {
-      println!("At {:?}", p);
-      p = tick(&env, &p)
+        println!("At {:?}", p);
+        p = tick(&env, &p)
     }
 
     println!("At {:?}", p);
