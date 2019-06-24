f27eb066a28a14c0fa9f87b4ceeb04560546ff31
Marc O'Morain <marc@circleci.com>
2019-06-24 22:35:50 +0100

    to_ppm

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,4 +1,5 @@
 use crate::tuple;
+use std::fmt::Write;
 
 #[derive(Debug)]
 pub struct Canvas {
@@ -18,6 +19,21 @@ pub fn canvas(width: usize, height: usize) -> Canvas {
     };
 }
 
+fn clamp(num: f32, min: f32, max: f32) -> f32 {
+    assert!(min <= max);
+    let mut x = num;
+    if x < min {
+        x = min;
+    }
+    if x > max {
+        x = max;
+    }
+    x
+}
+
+fn color(c: f32) -> i32 {
+    return clamp(255.0 * c, 0.0, 255.0).floor() as i32;
+}
 impl Canvas {
     pub fn write_pixel(&mut self, x: usize, y: usize, color: &tuple::Tuple) {
         self.data[(y * self.w) + x] = color.clone();
@@ -25,5 +41,26 @@ impl Canvas {
 
     pub fn pixel_at(&self, x: usize, y: usize) -> &tuple::Tuple {
         return &self.data[(y * self.w) + x];
+    }
+
+    pub fn to_ppm(&self) -> String {
+        let mut ppm = String::new();
+        write!(&mut ppm, "P3\n{} {}\n255\n", self.w, self.h).unwrap();
+        for y in 0..self.h {
+            for x in 0..self.w {
+                let pixel = &self.data[(self.w * y) + x];
+                write!(
+                    &mut ppm,
+                    "{} {} {}",
+                    color(pixel.0),
+                    color(pixel.1),
+                    color(pixel.2)
+                )
+                .unwrap();
+            }
+            ppm.push_str("\n");
+        }
+        ppm.push_str("\n");
+        return ppm;
     }
 }
